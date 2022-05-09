c60032ab27d3adba7cbca187396096586cf69632
6loodrain <89647718+6loodrain@users.noreply.github.com>
2022-05-09 19:28:07 +0500

    Update index.ts

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -4,14 +4,41 @@
  *	В хранилище можно вкладывать валюту через deposit(Currency)
  *	Из хранлилища, можно переводить валюту через transfer(Currency, Vault)
 */
+
 import { Currency } from "../task_1";
 
 export class Vault implements ISecureVaultRequisites{
 	public id: number;
 	public store: Set<Currency> = new Set<Currency>()
+
+	withdraw(currency: Currency): void {
+		const storedCurr = Array.from(this.store).find(x => x.name === currency.name);
+		if (!storedCurr || storedCurr.value < currency.value) {
+			throw new Error("Vault doesn't contains this currency");
+		} else {
+			storedCurr.value -= currency.value;
+		}
+	}
+
+	deposit(currency: Currency): void {
+		const storedCurr = Array.from(this.store).find(x => x.name === currency.name);
+		if (!storedCurr) {
+			this.store.add(currency);
+		} else {
+			storedCurr.value += currency.value;
+		}
+	}
+
+	transfer(currency: Currency, toVault: ISecureVaultRequisites): void {
+		this.withdraw(currency);
+		toVault.deposit(currency);
+	}
 }
 
 
 export interface ISecureVaultRequisites{
 	id: number
+	withdraw(currency: Currency): void;
+	deposit(currency: Currency): void;
+	transfer(currency: Currency, toVault: ISecureVaultRequisites): void;
 }
